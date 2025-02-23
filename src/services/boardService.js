8857fd6b1574ca724a9743cf4a39e3da8eaaf972
Kieuthai1 <kieuthai093@gmail.com>
2025-02-23 17:08:56 +0700

    api_create board & validate model

--- src/services/boardService.js
+++ src/services/boardService.js
@@ -1,6 +1,7 @@
 /* eslint-disable no-useless-catch */
 import { slugify } from '~/utils/formatters'
-const createNew = async(reqBody) => {
+import { boardModel } from '~/models/boardModel'
+const createNew = async (reqBody) => {
   try {
     // Xử lý logic dữ liệu tùy đặc thù dự án
     const newBoard = {
@@ -8,12 +9,16 @@ const createNew = async(reqBody) => {
       slug: slugify(reqBody.title)
     }
     // Gọi tới tầng Model để xử lý lưu bản ghi newBoard vào trong Database
-    // ..
+    const createdBoard = await boardModel.createNew(newBoard)
+
+    // Lấy bản ghi board sau khi gọi 
+    const getNewBoard = await boardModel.findOneById(createdBoard.insertedId)
 
     // Làm thêm các xử lý logic khác với các Collection khác tùy đặc thù dự án... vv
     // Bắn email, notification về cho admin khi 1 cái board mới được tạo...vv
 
-    return newBoard
+    // return createdBoard
+    return getNewBoard
   } catch (error) { throw error }
 }
 export const boardService = {
