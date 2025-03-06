74358f3ee7f64aa0b0e907c3119634a9c8515e42
Kieuthai1 <kieuthai093@gmail.com>
2025-03-06 07:23:25 +0700

    Update whitelist Domains

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,6 +1,7 @@
 // Những domain được phép truy cập tới tài nguyên của Server
 export const WHITELIST_DOMAINS = [
   // 'http://localhost:5173'
+  'https://trello-clone-phi-wheat.vercel.app/'
 ]
 export const BOARD_TYPES = {
   PUBLIC: 'public',
