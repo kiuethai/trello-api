8d927fd0bcce7f4198d6457f41885e3cfe703685
Kieuthai1 <kieuthai093@gmail.com>
2025-03-26 17:59:08 +0700

    add more origin in cors

--- src/config/cors.js
+++ src/config/cors.js
@@ -4,27 +4,42 @@ import { env } from '~/config/environment'
 import { StatusCodes } from 'http-status-codes'
 import ApiError from '~/utils/ApiError'
 
-// Cấu hình CORS Option trong dự án thực tế
 export const corsOptions = {
   origin: function (origin, callback) {
-    // Cho phép việc gọi API bằng POSTMAN trên môi trường dev,
-    // Thông thường khi sử dụng postman thì cái origin sẽ có giá trị là undefined
-    if ( env.BUILD_MODE === 'dev') {
+    // Các trường hợp được phép
+    const allowedOrigins = [
+      ...WHITELIST_DOMAINS,
+      'https://trello-api-1-3f5m.onrender.com',
+      'http://localhost',
+      null,
+      undefined
+    ]
+
+    // Kiểm tra origin
+    if (!origin || allowedOrigins.includes(origin)) {
       return callback(null, true)
     }
 
-    // Kiểm tra xem origin có phải là domain được chấp nhận hay không
-    if (WHITELIST_DOMAINS.includes(origin)) {
+    // Kiểm tra domain con hoặc domain gốc
+    const isAllowedDomain = allowedOrigins.some(allowedOrigin => {
+      try {
+        // So sánh hostname
+        return new URL(allowedOrigin).hostname === new URL(origin).hostname
+      } catch {
+        return false
+      }
+    })
+
+    if (isAllowedDomain) {
       return callback(null, true)
     }
 
-    // Cuối cùng nếu domain không được chấp nhận thì trả về lỗi
-    return callback(new ApiError(StatusCodes.FORBIDDEN, `${origin} not allowed by our CORS Policy.`))
+    // Nếu không được phép
+    return callback(new ApiError(
+      StatusCodes.FORBIDDEN, 
+      `${origin} not allowed by our CORS Policy.`
+    ))
   },
-
-  // Some legacy browsers (IE11, various SmartTVs) choke on 204
   optionsSuccessStatus: 200,
-
-  // CORS sẽ cho phép nhận cookies từ request,
   credentials: true
 }
